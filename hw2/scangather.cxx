b18eb544da8e04931b6b79530d4bf20a647ad6df
Gautam Rao <gautamrao@login4.frontera.tacc.utexas.edu>
2026-02-11 17:53:53 -0600

    HW2 submission

---
+++ hw2/scangather.cxx
@@ -0,0 +1,121 @@
+/****************************************************************
+ ****
+ **** This program file is part of the book 
+ **** `Parallel programming for Science and Engineering'
+ **** by Victor Eijkhout, eijkhout@tacc.utexas.edu
+ ****
+ **** copyright Victor Eijkhout 2012-2025
+ ****
+ **** MPI Exercise for the use of Scan/Exscan
+ ****
+ ****************************************************************/
+
+#include <iostream>
+#include <iomanip>
+#include <random>
+#include <sstream>
+#include <vector>
+#include <cmath>
+using namespace std;
+#include <mpi.h>
+
+#define max(a,b) ( (a)>(b) ? (a) : (b) )
+
+int main(int argc,char **argv) {
+
+  MPI_Comm comm;
+
+  MPI_Init(&argc,&argv);
+  comm = MPI_COMM_WORLD;
+  int nprocs,procno;
+  MPI_Comm_size(comm,&nprocs);
+  MPI_Comm_rank(comm,&procno);
+
+  stringstream proctext;
+
+  // first set a unique random seed
+  std::random_device rd;  // Used to get a random seed
+  std::mt19937 gen(rd() + procno);  // Mersenne Twister generator
+  std::uniform_int_distribution<> dis(0, nprocs-1);
+
+  /*
+   * How many elements do I have locally?
+   * Allocate space.
+   */
+  int
+    my_number_of_elements = dis(gen),
+    my_first_index=0;
+  /*
+   * Exercise:
+   * - determine the index of your first element.
+   *   Which routine do you use here?
+   */
+/**** your code here ****/
+    MPI_Exscan(&my_number_of_elements,&my_first_index,
+     1,MPI_INT,MPI_SUM,comm);
+    if (procno == 0) 
+      my_first_index = 0; 
+
+
+  printf("Proc %3d has %3d elements, range [%4d,%4d)\n",
+         procno,my_number_of_elements,my_first_index,my_first_index+my_number_of_elements);
+
+  /*
+   * Create a local array of size `my_number_of_elements'
+   * Fill in this local array
+   */
+  int total_number_of_elements;
+  vector<int> my_elements( my_number_of_elements );
+  for (int i_element=0; i_element<my_number_of_elements; i_element++)
+    my_elements[i_element] = my_first_index+i_element;
+
+  MPI_Reduce( &my_number_of_elements,&total_number_of_elements,
+	      1,MPI_INT,MPI_SUM,0,comm);
+  if (procno==0) {
+    printf("Total number of elements: %d\n",total_number_of_elements);
+  }
+
+  /*
+   * Gather all local arrays into one global
+   */
+  // how many elements from each process?
+  vector<int> size_buffer;
+  if (procno==0)
+    size_buffer = vector<int>( nprocs );
+    MPI_Gather(&my_number_of_elements,1, MPI_INT,
+	       size_buffer.data(), 1, MPI_INT, 0, comm); 
+  
+/**** your code here ****/
+
+  // where are they going to go in the big buffer?
+  vector<int> displ_buffer;
+  if (procno==0)
+    displ_buffer = vector<int>( nprocs );
+
+    MPI_Gather(&my_first_index,1, MPI_INT,
+               displ_buffer.data(), 1, MPI_INT, 0, comm);
+      
+  // now create the big buffer
+  vector<int> gather_buffer;
+  if (procno==0)
+    gather_buffer = vector<int>( total_number_of_elements );
+  /*
+   * Use Gatherv to collect the small buffers into a big one
+   */
+  MPI_Gatherv(my_elements.data(), my_number_of_elements,MPI_INT, gather_buffer.data(),
+		size_buffer.data(), displ_buffer.data(), MPI_INT, 0, comm);
+  
+
+  /*
+   * Print the gathered material
+   */
+  if (procno==0) {
+    printf("Gathered:");
+    for (int i_element=0; i_element<total_number_of_elements; i_element++)
+      printf(" %d",gather_buffer[i_element]);
+    printf("\n");
+  }
+
+  MPI_Finalize();
+  return 0;
+}
