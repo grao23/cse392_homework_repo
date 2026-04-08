9f5ec0559c8c07b2f706d0ee0353a3c704fb27f6
Gautam Rao <gautamrao@login1.frontera.tacc.utexas.edu>
2026-04-07 23:49:53 -0500

    HW8 Submission (Extension received)

---
+++ hw8/tasks.cxx
@@ -0,0 +1,81 @@
+#include <omp.h>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdlib>
+
+//reads input
+int read_input(int ifile) {
+  std::ifstream in("input" + std::to_string(ifile));
+  if (!in) {
+    std::cerr << "Input did not open" << ifile << "\n";
+    std::exit(1);
+  }
+
+  int value;
+  in >> value;
+  return value;
+}
+
+
+int transform(int *inputs, int ifile) {
+  int output = 0;
+  for (int iifile = 0; iifile <= ifile; iifile++) {
+    output += inputs[iifile];
+  }
+  return output;
+}
+
+//write integer out
+void writing_output(int ifile, int value) {
+  std::ofstream out("output" + std::to_string(ifile));
+  if (!out) {
+    std::cerr << "Could not open output" << ifile << "\n";
+    std::exit(1);
+  }
+
+  out << value << "\n";
+}
+
+int main() {
+  const int nfiles = 3;
+
+  std::vector<int> inputs(nfiles, 0);
+  std::vector<int> outputs(nfiles, 0);
+
+  int *inptr = inputs.data();
+  int *outptr = outputs.data();
+
+    #pragma omp parallel
+  {
+        #pragma omp single
+    {
+      for (int ifile = 0; ifile < nfiles; ifile++) {
+
+	// read inputs
+#pragma omp task firstprivate(ifile) depend(out: inptr[ifile])
+	{
+	  inptr[ifile] = read_input(ifile);
+	}
+
+	// output transformation
+#pragma omp task firstprivate(ifile) depend(in: inptr[0:ifile+1]) depend(out: outptr[ifile])
+	{
+	  outptr[ifile] = transform(inptr, ifile);
+	}
+
+	// write outputs
+#pragma omp task firstprivate(ifile) depend(in: outptr[ifile])
+	{
+	  writing_output(ifile, outptr[ifile]);
+	}
+      }
+
+            #pragma omp taskwait
+    }
+  }
+
+  std::cout << "done\n";
+  return 0;
+}
